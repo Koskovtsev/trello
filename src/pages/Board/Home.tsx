bf5b2ff8baa3ab690b82743cf5093d3f6324c6e3
Anton K <drvirus15@gmail.com>
2026-03-18 21:45:59 +0200

    api

--- src/pages/Board/Home.tsx
+++ src/pages/Board/Home.tsx
@@ -1,23 +1,32 @@
+// import { useEffect, useState } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+// import { AxiosResponse } from 'axios';
+// import api from '../../api/request';
 import { Board } from './components/Board/Board';
 import { IBoard } from '../../common/interfaces/IBoard';
 import './components/Board/board.scss';
 import './home.scss';
 
 export function Home(): JSX.Element {
-  const [boards] = useState<IBoard[]>([
+  const [items] = useState<IBoard[]>([
     { id: 1, title: 'покупки', custom: { background: 'red' } },
     { id: 2, title: 'підготовка до весілля', custom: { background: 'green' } },
     { id: 3, title: 'розробка інтернет-магазину', custom: { background: 'blue' } },
     { id: 4, title: 'курс по просуванню у соцмережах', custom: { background: 'grey' } },
   ]);
-
+  // useEffect(() => {
+  //   async function fetchData(): Promise<void> {
+  //     const { data } = await api.get('items');
+  //     setItems(data);
+  //   }
+  //   fetchData();
+  // }, []);
   return (
     <>
       <span className="home__page_title">Мої дошки</span>
       <div className="board__preview_list">
-        {boards.map((elem) => (
+        {items.map((elem) => (
           <Link key={elem.id} to={`/board/${elem.id}`}>
             <Board key={elem.id} id={elem.id} title={elem.title} custom={elem.custom} />
           </Link>
