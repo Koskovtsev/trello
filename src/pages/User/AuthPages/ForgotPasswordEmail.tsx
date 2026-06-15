6eba417fe88812de55c970fc4ef4b713f8e1b1ce
Anton K <drvirus15@gmail.com>
2026-06-15 17:27:16 +0300

    cosmetic fix

--- src/pages/User/AuthPages/ForgotPasswordEmail.tsx
+++ src/pages/User/AuthPages/ForgotPasswordEmail.tsx
@@ -1,4 +1,5 @@
 import toast from 'react-hot-toast';
+import { useState } from 'react';
 import { AuthButton } from './components/AuthButton/AuthButton';
 import { AuthEmailInput } from './components/AuthEmailInput/AuthEmailInput';
 import { AuthLayout } from './components/AuthLayout/AuthLayout';
@@ -7,6 +8,7 @@ import { authHelpers } from './utils/authHelpers';
 import { forgotPassword } from '../../../api/boardsService';
 
 export function ForgotPasswordEmail(): JSX.Element {
+  const [isEmailSent, setIsEmailSent] = useState(false);
   const { email, emailError, isValidEmail, handleEmailChange, handleEmailBlur } = useEmailValidation();
   const { parseBackendError } = authHelpers();
   const handleSubmit = async (event: React.FormEvent): Promise<void> => {
@@ -21,13 +23,25 @@ export function ForgotPasswordEmail(): JSX.Element {
         email: email.trim().toLocaleLowerCase(),
       };
       await forgotPassword(payload);
+      setIsEmailSent(true);
     } catch (error) {
       toast.error(parseBackendError(error, 'Помилка при відправці листа'));
     }
   };
   return (
-    <AuthLayout title="Введіть пошту" footer={<AuthButton>Відправити посилання</AuthButton>} onSubmit={handleSubmit}>
-      <AuthEmailInput value={email} onChange={handleEmailChange} emailError={emailError} onBlur={handleEmailBlur} />
+    <AuthLayout
+      title={isEmailSent ? 'Готово' : 'Введіть пошту'}
+      footer={!isEmailSent && <AuthButton>Відправити посилання</AuthButton>}
+      onSubmit={handleSubmit}
+    >
+      {!isEmailSent ? (
+        <AuthEmailInput value={email} onChange={handleEmailChange} emailError={emailError} onBlur={handleEmailBlur} />
+      ) : (
+        <div className="auth__success-message">
+          Якщо обліковий запис із такою електронною поштою існує, ми надіслали лист для відновлення пароля. Перевірте
+          електронну пошту та перейдіть за посиланням для відновлення пароля.
+        </div>
+      )}
     </AuthLayout>
   );
 }
