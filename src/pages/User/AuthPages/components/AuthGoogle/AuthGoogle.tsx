8d09a650ad99ec0788a32d53567211f219bac6d5
Anton K <drvirus15@gmail.com>
2026-06-22 20:02:54 +0300

    OAuth fix

--- src/pages/User/AuthPages/components/AuthGoogle/AuthGoogle.tsx
+++ src/pages/User/AuthPages/components/AuthGoogle/AuthGoogle.tsx
@@ -3,7 +3,7 @@ import GoogleIcon from '../../../../../assets/google-icon-logo.svg';
 
 export function AuthGoogle(): JSX.Element {
   const handleGoogleAuth = (): void => {
-    window.location.assign(`${process.env.REACT_APP_API_URL}/auth/google`);
+    window.location.assign(`${process.env.REACT_APP_API_URL}auth/google`);
   };
   return (
     <AuthButton type="button" variant="google" onClick={handleGoogleAuth} icon={<img src={GoogleIcon} alt="Google" />}>
